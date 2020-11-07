0090309f02bbe48234168f1e44c0e229078295e3
julijanjug <julijan.jug@gmail.com>
2020-11-07 22:31:31 +0100

    ss

--- frontend/forest2tp-frontend/src/app/map/map.component.ts
+++ frontend/forest2tp-frontend/src/app/map/map.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
 import FreeDraw, { DELETE, EDIT, NONE, CREATE } from 'leaflet-freedraw';
 import 'leaflet-easybutton';
-
+import  'leaflet-search';
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -21,23 +21,30 @@ export class MapComponent implements OnInit {
   }
 
   private initMap(): void {
+    //search box
+    var searchLayer = L.geoJson();
+
     this.map = L.map('map', {
       center: [ 39.8282, -98.5795 ],
       zoom: 3,
-      doubleClickZoom: false
-    }).locate({setView: true, maxZoom: 13});;
+      doubleClickZoom: false,
+      searchControl: {layer: searchLayer}
+    }).locate({setView: true, maxZoom: 13});
 
-    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    });
-    tiles.addTo(this.map);
+    //satelite imagery
+    var mapLink = '<a href="http://www.esri.com/">Esri</a>';
+    var wholink = 'i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community';
+    L.tileLayer(
+    'http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    attribution: '&copy; '+mapLink+', '+wholink,
+    maxZoom: 19,
+    }).addTo(this.map);
 
     //add drawing functionality
     const freeDraw = new FreeDraw({
         mode: FreeDraw.ALL,
         smoothFactor: 0.7,
-        simplifyFactor: 1.5,
+        simplifyFactor: 1.4,
         strokeWidth: 3,
         maximumPolygons: 1
     });
@@ -48,7 +55,7 @@ export class MapComponent implements OnInit {
     });
 
     //Toolbar for draw control
-    var stateChangingButton = L.easyButton({
+    var stateChangingButton = L.easyButton({ 
         states: [{
                 stateName: 'enable-add-polygon',        // name the state
                 icon:      '<i class="fas fa-draw-polygon"></i>',               // and define its properties
@@ -68,13 +75,5 @@ export class MapComponent implements OnInit {
         }]
     });
     stateChangingButton.addTo(this.map);
-
-    var mapLink = '<a href="http://www.esri.com/">Esri</a>';
-    var wholink = 'i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community';
-    L.tileLayer(
-    'http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-    attribution: '&copy; '+mapLink+', '+wholink,
-    maxZoom: 18,
-    }).addTo(this.map);
   }
 }
