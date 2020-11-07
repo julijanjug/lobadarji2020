14d6a52b453657c65f6460e70399df64b07efbfe
ckevin <kevin.cvetezar@comtrade.com>
2020-11-07 23:00:06 +0100

    implemented count function

--- backend/api/wcPaper/wcPaper.js
+++ backend/api/wcPaper/wcPaper.js
@@ -1,21 +1,31 @@
  const got = require('got');
  var getPixels = require("get-pixels")
+ const util = require('util');
+ const getPixelsAsync = util.promisify(getPixels);
 
 
+  [
+    15.255205,
+    46.569955
+  ],
+  [
+    15.284724,
+    46.539263
+  ]
+  46.496974
 const poly = [
-    {lat: 46.05809692060124, lng: 14.832648038864138},
-     {lat: 46.057858678604, lng: 14.832787513732912},
-    {lat: 46.057858678604, lng: 14.832755327224731},
-    {lat: 46.05778422776906, lng: 14.832551479339601},
-    {lat: 46.05777678268005, lng: 14.832487106323244},
-    {lat: 46.0576502160132, lng: 14.832133054733278},
-    {lat: 46.05809692060124, lng: 14.832648038864138}
+    {lat: 46.539263, lng: 15.284724},
+     {lat: 46.496974, lng: 15.351313},
+    {lat: 46.532178, lng: 15.378086},
+    {lat: 46.571843, lng: 15.304632},
+    {lat: 46.569955, lng: 15.255205},
+    {lat: 46.539263, lng: 15.284724},
 ]
 
  const requestBuilderWMS = async (polygon) => {
      let string1 = ''
      polygon.forEach(element => {
-         string1+= `${element.lat} ${element.lat},`
+         string1+= `${element.lng} ${element.lat},`
      });
      string1 = string1.substring(0, string1.length - 1);
      console.log('string', string1)
@@ -24,8 +34,16 @@ const poly = [
      let iterator = {}
      let count = 1
      const rgb = []
-     getPixels(`https://services.sentinel-hub.com/ogc/wms/${apiString}`, function(err, pixels) {
-         
+     const result = await getPixelsAsync(`https://services.sentinel-hub.com/ogc/wms/${apiString}`)
+     console.log('`string ', `https://services.sentinel-hub.com/ogc/wms/${apiString}`);
+     iterator = result.data.values()
+     for (i = 0; i < result.data.length; i+=4) {
+         const tmp = [iterator.next().value,iterator.next().value, iterator.next().value, iterator.next().value]
+         rgb.push(tmp)
+       } 
+       console.log('rgb: ');
+     return rgb
+    /* await getPixels(`https://services.sentinel-hub.com/ogc/wms/${apiString}`, function(err, pixels) {
          if(err) {
              console.log("Bad image path")
             }
@@ -34,21 +52,36 @@ const poly = [
                 const tmp = [iterator.next().value,iterator.next().value, iterator.next().value, iterator.next().value]
                 rgb.push(tmp)
               } 
-              console.log('rgb: ', rgb);
-              return rgb
-      })
+              console.log('rgb: ');
+            return rgb
+      })*/
     //const response = await got(apiString, {prefixUrl: 'https://services.sentinel-hub.com/ogc/wms/'});
  }
+
  
+
+ const areaCalucation = async (pixels) => {
+     console.log('pixels: ', pixels);
+    
+    let count = 0
+     pixels.forEach(pixel => {
+
+        if(pixel[0] > 40 && pixel[0] < 60 && pixel[1] > 200 && pixel[2] > 200) count++;
+
+     })
+     console.log('count: ', count);
+     return count
+ }
 
 exports.getArea = async (req, res) => {
     console.log("request recieved")
     try {
         const response = await requestBuilderWMS(poly)
+        console.log('response: ');
         const area = await areaCalucation(response)
-        console.log('response: ', response);
+        console.log('area: ', area);
 
-        return res.status(200).send(response)
+        return res.json({'number': area})
     } catch (error) {
         console.log(error)
         return res.status(404).send('Error occured while .', error)
