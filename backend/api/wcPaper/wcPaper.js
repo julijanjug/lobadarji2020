fe3c9266edc93755f7253ffcef20df92b306d597
ckevin <kevin.cvetezar@comtrade.com>
2020-11-08 01:56:07 +0100

    Update helper to fit new geocordinates

--- backend/api/wcPaper/wcPaper.js
+++ backend/api/wcPaper/wcPaper.js
@@ -5,28 +5,20 @@
  const { getAream2 } = require('./helper');
 
 
-  [
-    15.255205,
-    46.569955
-  ],
-  [
-    15.284724,
-    46.539263
-  ]
-  46.496974
+
 const poly = [
-    {lat: 46.539263, lng: 15.284724},
-     {lat: 46.496974, lng: 15.351313},
-    {lat: 46.532178, lng: 15.378086},
-    {lat: 46.571843, lng: 15.304632},
-    {lat: 46.569955, lng: 15.255205},
-    {lat: 46.539263, lng: 15.284724},
+    [46.539263, 15.284724],
+     [46.496974, 15.351313],
+    [46.532178, 15.378086],
+    [46.571843, 15.304632],
+    [46.569955, 15.255205],
+    [46.539263, 15.284724],
 ]
 
  const requestBuilderWMS = async (polygon) => {
      let string1 = ''
      polygon.forEach(element => {
-         string1+= `${element.lng} ${element.lat},`
+         string1+= `${element[1]} ${element[0]},`
      });
      string1 = string1.substring(0, string1.length - 1);
      // hardcoded instance id, beacuse im too tired to put it in env, sry whoever is reading this.
@@ -48,21 +40,24 @@ const poly = [
 
  const areaCalucation = async (pixels, area) => {
     let forestPixel = 0
-    let all = 0
     let notPoligon = 0
+    let all = 0
      pixels.forEach(pixel => {
 
         if(pixel[0] > 40 && pixel[0] < 60 && pixel[1] > 200 && pixel[2] > 200) forestPixel++;
         if(pixel[0] < 60 && pixel[1] > 85 && pixel[1] < 140 && pixel[2] > 85 && pixel[2] < 140) forestPixel++;
-        all++
         // find white and black pixels.
         if((pixel[0] === 255 && pixel[2] === 255 && pixel[1] === 255)
         || (pixel[2] < 10 && pixel[1] < 10)) {
             notPoligon++;
         }
+        all++;
      })
-     const ration = forestPixel/(1-notPoligon)
-     return forestPixel*area
+     const ratio = forestPixel/(all-(notPoligon))
+     
+     console.log('area: ', area);
+     console.log('ratio: ', ratio);
+     return ratio*area
  }
 
 exports.getArea = async (req, res) => {
@@ -73,6 +68,7 @@ exports.getArea = async (req, res) => {
         const allArea = getAream2(poly)
 
         const area = await areaCalucation(rgb, allArea)
+        console.log('area: ', area);
         
         return res.json({'m2': area, 'uri': apiString})
     } catch (error) {
